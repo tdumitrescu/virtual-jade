a3eda1100241e53bb671704e926a83f2ace25e89
t ddddddd <webdev@cmme.org>
2016-12-03 18:00:36 -0800

    convert html.includes assertions to expect.js

--- test/test.js
+++ test/test.js
@@ -108,9 +108,9 @@ describe(`configuration`, function() {
 
   it(`should render a template without options`, function() {
     const compiled = render(fixture(`attributes`));
-    assert(compiled.includes(`class1`));
-    assert(compiled.includes(`foo`));
-    assert(compiled.includes(`doge`));
+    expect(compiled).to.contain(`class1`);
+    expect(compiled).to.contain(`foo`);
+    expect(compiled).to.contain(`doge`);
   });
 });
 
@@ -155,9 +155,9 @@ for (let vdom of VDOM_LIBS) {
 
     it(`should insert dynamic tag names`, function() {
       let html = renderFixture(`dynamic-tag`, {myTag: `input`});
-      assert(html.includes(`<input class="llamas">`));
+      expect(html).to.contain(`<input class="llamas">`);
       html = renderFixture(`dynamic-tag`, {myTag: `textarea`});
-      assert(html.includes(`<textarea class="llamas">`));
+      expect(html).to.contain(`<textarea class="llamas">`);
     });
 
     it(`renders a simple template`, function() {
@@ -167,8 +167,8 @@ for (let vdom of VDOM_LIBS) {
 
     it(`translates basic class notation`, function() {
       const html = renderFixture(`class`);
-      assert(html.includes(`div class="foo"`));
-      assert(html.includes(`div class="baz llamas"`));
+      expect(html).to.contain(`div class="foo"`);
+      expect(html).to.contain(`div class="baz llamas"`);
     });
 
     describe(`iteration`, function() {
@@ -186,111 +186,111 @@ for (let vdom of VDOM_LIBS) {
     describe(`multiple files`, function() {
       it(`should insert included files`, function() {
         const html = renderFixture(`include`);
-        assert(html.includes(`<p>Hello</p>`));
-        assert(html.includes(`<div class="included-content">llamas!!!</div>`));
-        assert(html.includes(`<p>world</p>`));
+        expect(html).to.contain(`<p>Hello</p>`);
+        expect(html).to.contain(`<div class="included-content">llamas!!!</div>`);
+        expect(html).to.contain(`<p>world</p>`);
       });
 
       it(`should make included mixins available`, function() {
         const html = renderFixture(`include-with-mixin`);
-        assert(html.includes(`<div class="foo">`));
-        assert(html.includes(`<div class="hello">insert me</div>`));
+        expect(html).to.contain(`<div class="foo">`);
+        expect(html).to.contain(`<div class="hello">insert me</div>`);
       });
 
       it(`should insert extended files`, function() {
         const html = renderFixture(`extends`);
-        assert(html.includes(`<div class="foo">`));
-        assert(html.includes(`capybara`));
-        assert(html.includes(`default content`));
+        expect(html).to.contain(`<div class="foo">`);
+        expect(html).to.contain(`capybara`);
+        expect(html).to.contain(`default content`);
       });
     });
 
     describe(`attributes`, function() {
       it(`should add arbitrary attributes`, function() {
         const html = renderFixture(`attributes`);
-        assert(html.includes(`required`));
-        assert(!html.includes(`something`) || html.includes(`something="false"`));
+        expect(html).to.contain(`required`);
+        expect(!html.includes(`something`) || html.includes(`something="false"`)).to.be.ok();
       });
 
       it(`should add class attributes in array notation`, function() {
         let html = renderFixture(`attributes`);
-        assert(html.includes(`1 2`));
+        expect(html).to.contain(`1 2`);
         html = renderFixture(`attributes`, {variable: `meow`});
-        assert(html.includes(`1 2 meow`));
+        expect(html).to.contain(`1 2 meow`);
       });
 
       it(`should add class attributes in object notation`, function() {
         let html = renderFixture(`attributes`);
-        assert(html.includes(`obj1`));
-        assert(!html.includes(`obj2`));
+        expect(html).to.contain(`obj1`);
+        expect(html).not.to.contain(`obj2`);
 
         html = renderFixture(`attributes`, {variable: `doge`});
-        assert(html.includes(`obj1`));
-        assert(html.includes(`obj2`));
+        expect(html).to.contain(`obj1`);
+        expect(html).to.contain(`obj2`);
       });
 
       it(`should combine class attributes in different notations gracefully`, function() {
         let html = renderFixture(`attributes`);
-        assert(html.includes(`mixed`));
-        assert(html.includes(`mixedArray1`));
-        assert(!html.includes(`mixedObj1`));
+        expect(html).to.contain(`mixed`);
+        expect(html).to.contain(`mixedArray1`);
+        expect(html).not.to.contain(`mixedObj1`);
 
         html = renderFixture(`attributes`, {var2: `doge`});
-        assert(html.includes(`mixed`));
-        assert(html.includes(`mixedArray1`));
-        assert(html.includes(`mixedObj1`));
-        assert(html.includes(`doge`));
+        expect(html).to.contain(`mixed`);
+        expect(html).to.contain(`mixedArray1`);
+        expect(html).to.contain(`mixedObj1`);
+        expect(html).to.contain(`doge`);
       });
 
       it(`should render data attributes`, function() {
         const html = renderFixture(`attributes`, {variable: `capybara`});
-        assert(html.includes(`data-foo-id="42"`));
-        assert(html.includes(`data-var="capybara"`));
+        expect(html).to.contain(`data-foo-id="42"`);
+        expect(html).to.contain(`data-var="capybara"`);
       });
 
       it(`should convert attributes to correct property names`, function() {
         const html = renderFixture(`attributes`);
-        assert(html.includes(`autocomplete="on"`));
-        assert(html.includes(`tabindex="5"`));
-        assert(html.includes(`for="special-attr"`));
+        expect(html).to.contain(`autocomplete="on"`);
+        expect(html).to.contain(`tabindex="5"`);
+        expect(html).to.contain(`for="special-attr"`);
       });
     });
 
     describe(`case statements`, function() {
       it(`should not execute any cases when none match`, function() {
         const html = renderFixture(`case`);
-        assert(!html.includes(`foo`));
-        assert(!html.includes(`bar`));
+        expect(html).not.to.contain(`foo`);
+        expect(html).not.to.contain(`bar`);
       });
 
       it(`should execute default when no others match`, function() {
         const html = renderFixture(`case`);
-        assert(html.includes(`llama`));
+        expect(html).to.contain(`llama`);
       });
 
       it(`should execute only one match`, function() {
         const html = renderFixture(`case`, {variable: 1});
-        assert(html.includes(`span`));
-        assert(!html.includes(`input`));
-        assert(!html.includes(`textarea`));
+        expect(html).to.contain(`span`);
+        expect(html).not.to.contain(`input`);
+        expect(html).not.to.contain(`textarea`);
       });
 
       it(`should fall through from the first case in a chain`, function() {
         const html = renderFixture(`case`, {variable2: `d`});
-        assert(html.includes(`bar`));
-        assert(!html.includes(`foo`));
+        expect(html).to.contain(`bar`);
+        expect(html).not.to.contain(`foo`);
       });
 
       it(`should fall through from the middle case in a chain`, function() {
         const html = renderFixture(`case`, {variable2: `e`});
-        assert(html.includes(`bar`));
-        assert(!html.includes(`foo`));
+        expect(html).to.contain(`bar`);
+        expect(html).not.to.contain(`foo`);
       });
 
       it(`should fall through from the last case in a chain`, function() {
         const html = renderFixture(`case`, {variable2: `f`});
-        assert(html.includes(`bar`));
-        assert(!html.includes(`foo`));
+        expect(html).to.contain(`bar`);
+        expect(html).not.to.contain(`foo`);
       });
     });
 
@@ -314,15 +314,15 @@ for (let vdom of VDOM_LIBS) {
       });
 
       it(`should output inline buffered code`, function() {
-        assert(html.includes(`<div class="inline-script"><div class="raw-inline">within another div`));
+        expect(html).to.contain(`<div class="inline-script"><div class="raw-inline">within another div`);
       });
 
       it(`should output standalone buffered code`, function() {
-        assert(html.includes(`<div class="raw-buffered">raw so raw`));
+        expect(html).to.contain(`<div class="raw-buffered">raw so raw`);
       });
 
       it(`should not output unbuffered code`, function() {
-        assert(!html.includes(`should not be output`));
+        expect(html).not.to.contain(`should not be output`);
       });
     });
   });
