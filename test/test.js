ba5d645c69552ef1fd3795b73cd5bc3c1a22fa38
teddddd <ted@mixpanel.com>
2016-12-26 13:07:14 -0500

    test literal includes against snabbdom (failingly)

--- test/test.js
+++ test/test.js
@@ -87,219 +87,250 @@ describe(`configuration`, function() {
   });
 });
 
+describe(`rendering`, function() {
 
-// run rendering tests against each supported Virtual DOM library
+  jsdom();
 
-const VDOM_LIBS = [
-  `virtual-dom`,
-  `snabbdom`,
-];
+  // run rendering tests against each supported Virtual DOM library
 
-for (let vdom of VDOM_LIBS) {
-  const renderFixture = function(fixtureName, locals) {
-    return fixtureToHTML(fixtureName, locals, {vdom, pretty: true});
-  };
+  const VDOM_LIBS = [
+    `virtual-dom`,
+    `snabbdom`,
+  ];
 
-  describe(`rendering with ${vdom}`, function() {
-    it(`renders a template`, function() {
-      const html = renderFixture(`item`, {
-        item: {
-          id: `1234`,
-          title: `some title`,
-          description: `some description`,
-          active: true,
-        },
+  for (let vdom of VDOM_LIBS) {
+    const renderFixture = function(fixtureName, locals) {
+      return fixtureToHTML(fixtureName, locals, {vdom, pretty: true});
+    };
+
+    context(`using library "${vdom}"`, function() {
+      it(`renders a template`, function() {
+        const html = renderFixture(`item`, {
+          item: {
+            id: `1234`,
+            title: `some title`,
+            description: `some description`,
+            active: true,
+          },
+        });
+
+        expect(html).to.contain(`item active`);
+        expect(html).to.contain(`class="title"`);
+        expect(html).to.contain(`<h3`);
+        expect(html).to.contain(`</h3>`);
+        expect(html).to.contain(`some title`);
+        expect(html).to.contain(`some description`);
       });
 
-      expect(html).to.contain(`item active`);
-      expect(html).to.contain(`class="title"`);
-      expect(html).to.contain(`<h3`);
-      expect(html).to.contain(`</h3>`);
-      expect(html).to.contain(`some title`);
-      expect(html).to.contain(`some description`);
+      it(`beautifies when option is set`, function() {
+        const fn = render(fixture(`item`), {pretty: true});
+        const lines = fn.split(`\n`);
+        expect(lines.length).to.be.greaterThan(15);
+        expect(lines[lines.length - 1].trim()).to.be(`}`);
+      });
+
+      it(`inserts dynamic tag names`, function() {
+        let html = renderFixture(`dynamic-tag`, {myTag: `input`});
+        expect(html).to.contain(`<input class="llamas">`);
+        html = renderFixture(`dynamic-tag`, {myTag: `textarea`});
+        expect(html).to.contain(`<textarea class="llamas">`);
+      });
+
+      it(`renders a simple template`, function() {
+        const html = renderFixture(`simple`);
+        expect(html).to.be(`<div>This is text</div>`);
+      });
+
+      it(`translates basic class notation`, function() {
+        const html = renderFixture(`class`);
+        expect(html).to.contain(`div class="foo"`);
+        expect(html).to.contain(`div class="baz llamas"`);
+      });
+
+      describe(`iteration`, function() {
+        it(`runs "each" loops correctly`, function() {
+          const html = renderFixture(`each-expression`, {values: [`foo`, `bar`]});
+          expect(html).to.contain(`<li>foo</li><li>bar</li>`);
+        });
+
+        it(`runs "while" loops correctly`, function() {
+          const html = renderFixture(`while`);
+          expect(html.match(/<div class=\"item\">/g)).to.have.length(5);
+        });
+      });
+
+      describe(`multiple files`, function() {
+        it(`inserts included files`, function() {
+          const html = renderFixture(`include`);
+          expect(html).to.contain(`<p>Hello</p>`);
+          expect(html).to.contain(`<div class="included-content">llamas!!!</div>`);
+          expect(html).to.contain(`<p>world</p>`);
+        });
+
+        it(`makes included mixins available`, function() {
+          const html = renderFixture(`include-with-mixin`);
+          expect(html).to.contain(`<div class="foo">`);
+          expect(html).to.contain(`<div class="hello">insert me</div>`);
+        });
+
+        it(`inserts extended files`, function() {
+          const html = renderFixture(`extends`);
+          expect(html).to.contain(`<div class="foo">`);
+          expect(html).to.contain(`capybara`);
+          expect(html).to.contain(`default content`);
+        });
+      });
+
+      describe(`attributes`, function() {
+        it(`adds arbitrary attributes`, function() {
+          const html = renderFixture(`attributes`);
+          expect(html).to.contain(`required`);
+          expect(!html.includes(`something`) || html.includes(`something="false"`)).to.be.ok();
+        });
+
+        it(`adds class attributes in array notation`, function() {
+          let html = renderFixture(`attributes`);
+          expect(html).to.contain(`1 2`);
+          html = renderFixture(`attributes`, {variable: `meow`});
+          expect(html).to.contain(`1 2 meow`);
+        });
+
+        it(`adds class attributes in object notation`, function() {
+          let html = renderFixture(`attributes`);
+          expect(html).to.contain(`obj1`);
+          expect(html).not.to.contain(`obj2`);
+
+          html = renderFixture(`attributes`, {variable: `doge`});
+          expect(html).to.contain(`obj1`);
+          expect(html).to.contain(`obj2`);
+        });
+
+        it(`combines class attributes in different notations gracefully`, function() {
+          let html = renderFixture(`attributes`);
+          expect(html).to.contain(`mixed`);
+          expect(html).to.contain(`mixedArray1`);
+          expect(html).not.to.contain(`mixedObj1`);
+
+          html = renderFixture(`attributes`, {var2: `doge`});
+          expect(html).to.contain(`mixed`);
+          expect(html).to.contain(`mixedArray1`);
+          expect(html).to.contain(`mixedObj1`);
+          expect(html).to.contain(`doge`);
+        });
+
+        it(`renders data attributes`, function() {
+          const html = renderFixture(`attributes`, {variable: `capybara`});
+          expect(html).to.contain(`data-foo-id="42"`);
+          expect(html).to.contain(`data-var="capybara"`);
+        });
+
+        it(`converts attributes to correct property names`, function() {
+          const html = renderFixture(`attributes`);
+          expect(html).to.contain(`autocomplete="on"`);
+          expect(html).to.contain(`tabindex="5"`);
+          expect(html).to.contain(`for="special-attr"`);
+        });
+      });
+
+      describe(`case statements`, function() {
+        it(`does not execute any cases when none match`, function() {
+          const html = renderFixture(`case`);
+          expect(html).not.to.contain(`foo`);
+          expect(html).not.to.contain(`bar`);
+        });
+
+        it(`executes default when no others match`, function() {
+          const html = renderFixture(`case`);
+          expect(html).to.contain(`llama`);
+        });
+
+        it(`executes only one match`, function() {
+          const html = renderFixture(`case`, {variable: 1});
+          expect(html).to.contain(`span`);
+          expect(html).not.to.contain(`input`);
+          expect(html).not.to.contain(`textarea`);
+        });
+
+        it(`falls through from the first case in a chain`, function() {
+          const html = renderFixture(`case`, {variable2: `d`});
+          expect(html).to.contain(`bar`);
+          expect(html).not.to.contain(`foo`);
+        });
+
+        it(`falls through from the middle case in a chain`, function() {
+          const html = renderFixture(`case`, {variable2: `e`});
+          expect(html).to.contain(`bar`);
+          expect(html).not.to.contain(`foo`);
+        });
+
+        it(`falls through from the last case in a chain`, function() {
+          const html = renderFixture(`case`, {variable2: `f`});
+          expect(html).to.contain(`bar`);
+          expect(html).not.to.contain(`foo`);
+        });
+      });
+
+      describe(`code blocks`, function() {
+        let html;
+
+        beforeEach(function() {
+          html = renderFixture(`code`);
+        });
+
+        it(`runs unbuffered code correctly`, function() {
+          expect(html).to.contain(`<div class="example bar">`);
+        });
+
+        it(`uses locals when evaluating`, function() {
+          html = renderFixture(`code`, {x: 0});
+          expect(html).to.contain(`<div class="baz">`);
+
+          html = renderFixture(`code`, {x: -1});
+          expect(html).not.to.contain(`<div class="baz">`);
+        });
+
+        it(`outputs inline buffered code`, function() {
+          expect(html).to.contain(`<div class="inline-script"><div class="raw-inline">within another div`);
+        });
+
+        it(`outputs standalone buffered code`, function() {
+          expect(html).to.contain(`<div class="raw-buffered">raw so raw`);
+        });
+
+        it(`does not output unbuffered code`, function() {
+          expect(html).not.to.contain(`should not be output`);
+        });
+      });
+
+      describe(`multiple files`, function() {
+        const htmlEntities = function(str) {
+          return String(str)
+            .replace(/&/g, `&amp;`)
+            .replace(/</g, `&lt;`)
+            .replace(/>/g, `&gt;`)
+            .replace(/"/g, `&quot;`)
+            ;
+        };
+
+        it(`inserts included literal (non-jade) files`, function() {
+          const html = renderFixture(`literal-import`);
+          const singleRootImport = `<div class="test">test</div>`;
+          const multiRootImport = `<div>child 1</div><div>child 2</div>`;
+          expect(html).to.be(
+            `<div class="raw">` +
+              `<div class="single-root">` +
+                `<text>` + htmlEntities(singleRootImport) + `</text>` +
+              `</div>` +
+              `<div class="multi-root">` +
+                `<text>` + htmlEntities(`<div>` + multiRootImport + `</div>`) + `</text>` +
+              `</div>` +
+            `</div>`
+          );
+        });
+      });
     });
-
-    it(`beautifies when option is set`, function() {
-      const fn = render(fixture(`item`), {pretty: true});
-      const lines = fn.split(`\n`);
-      expect(lines.length).to.be.greaterThan(15);
-      expect(lines[lines.length - 1].trim()).to.be(`}`);
-    });
-
-    it(`inserts dynamic tag names`, function() {
-      let html = renderFixture(`dynamic-tag`, {myTag: `input`});
-      expect(html).to.contain(`<input class="llamas">`);
-      html = renderFixture(`dynamic-tag`, {myTag: `textarea`});
-      expect(html).to.contain(`<textarea class="llamas">`);
-    });
-
-    it(`renders a simple template`, function() {
-      const html = renderFixture(`simple`);
-      expect(html).to.be(`<div>This is text</div>`);
-    });
-
-    it(`translates basic class notation`, function() {
-      const html = renderFixture(`class`);
-      expect(html).to.contain(`div class="foo"`);
-      expect(html).to.contain(`div class="baz llamas"`);
-    });
-
-    describe(`iteration`, function() {
-      it(`runs "each" loops correctly`, function() {
-        const html = renderFixture(`each-expression`, {values: [`foo`, `bar`]});
-        expect(html).to.contain(`<li>foo</li><li>bar</li>`);
-      });
-
-      it(`runs "while" loops correctly`, function() {
-        const html = renderFixture(`while`);
-        expect(html.match(/<div class=\"item\">/g)).to.have.length(5);
-      });
-    });
-
-    describe(`multiple files`, function() {
-      it(`inserts included files`, function() {
-        const html = renderFixture(`include`);
-        expect(html).to.contain(`<p>Hello</p>`);
-        expect(html).to.contain(`<div class="included-content">llamas!!!</div>`);
-        expect(html).to.contain(`<p>world</p>`);
-      });
-
-      it(`makes included mixins available`, function() {
-        const html = renderFixture(`include-with-mixin`);
-        expect(html).to.contain(`<div class="foo">`);
-        expect(html).to.contain(`<div class="hello">insert me</div>`);
-      });
-
-      it(`inserts extended files`, function() {
-        const html = renderFixture(`extends`);
-        expect(html).to.contain(`<div class="foo">`);
-        expect(html).to.contain(`capybara`);
-        expect(html).to.contain(`default content`);
-      });
-    });
-
-    describe(`attributes`, function() {
-      it(`adds arbitrary attributes`, function() {
-        const html = renderFixture(`attributes`);
-        expect(html).to.contain(`required`);
-        expect(!html.includes(`something`) || html.includes(`something="false"`)).to.be.ok();
-      });
-
-      it(`adds class attributes in array notation`, function() {
-        let html = renderFixture(`attributes`);
-        expect(html).to.contain(`1 2`);
-        html = renderFixture(`attributes`, {variable: `meow`});
-        expect(html).to.contain(`1 2 meow`);
-      });
-
-      it(`adds class attributes in object notation`, function() {
-        let html = renderFixture(`attributes`);
-        expect(html).to.contain(`obj1`);
-        expect(html).not.to.contain(`obj2`);
-
-        html = renderFixture(`attributes`, {variable: `doge`});
-        expect(html).to.contain(`obj1`);
-        expect(html).to.contain(`obj2`);
-      });
-
-      it(`combines class attributes in different notations gracefully`, function() {
-        let html = renderFixture(`attributes`);
-        expect(html).to.contain(`mixed`);
-        expect(html).to.contain(`mixedArray1`);
-        expect(html).not.to.contain(`mixedObj1`);
-
-        html = renderFixture(`attributes`, {var2: `doge`});
-        expect(html).to.contain(`mixed`);
-        expect(html).to.contain(`mixedArray1`);
-        expect(html).to.contain(`mixedObj1`);
-        expect(html).to.contain(`doge`);
-      });
-
-      it(`renders data attributes`, function() {
-        const html = renderFixture(`attributes`, {variable: `capybara`});
-        expect(html).to.contain(`data-foo-id="42"`);
-        expect(html).to.contain(`data-var="capybara"`);
-      });
-
-      it(`converts attributes to correct property names`, function() {
-        const html = renderFixture(`attributes`);
-        expect(html).to.contain(`autocomplete="on"`);
-        expect(html).to.contain(`tabindex="5"`);
-        expect(html).to.contain(`for="special-attr"`);
-      });
-    });
-
-    describe(`case statements`, function() {
-      it(`does not execute any cases when none match`, function() {
-        const html = renderFixture(`case`);
-        expect(html).not.to.contain(`foo`);
-        expect(html).not.to.contain(`bar`);
-      });
-
-      it(`executes default when no others match`, function() {
-        const html = renderFixture(`case`);
-        expect(html).to.contain(`llama`);
-      });
-
-      it(`executes only one match`, function() {
-        const html = renderFixture(`case`, {variable: 1});
-        expect(html).to.contain(`span`);
-        expect(html).not.to.contain(`input`);
-        expect(html).not.to.contain(`textarea`);
-      });
-
-      it(`falls through from the first case in a chain`, function() {
-        const html = renderFixture(`case`, {variable2: `d`});
-        expect(html).to.contain(`bar`);
-        expect(html).not.to.contain(`foo`);
-      });
-
-      it(`falls through from the middle case in a chain`, function() {
-        const html = renderFixture(`case`, {variable2: `e`});
-        expect(html).to.contain(`bar`);
-        expect(html).not.to.contain(`foo`);
-      });
-
-      it(`falls through from the last case in a chain`, function() {
-        const html = renderFixture(`case`, {variable2: `f`});
-        expect(html).to.contain(`bar`);
-        expect(html).not.to.contain(`foo`);
-      });
-    });
-
-    describe(`code blocks`, function() {
-      let html;
-
-      beforeEach(function() {
-        html = renderFixture(`code`);
-      });
-
-      it(`runs unbuffered code correctly`, function() {
-        expect(html).to.contain(`<div class="example bar">`);
-      });
-
-      it(`uses locals when evaluating`, function() {
-        html = renderFixture(`code`, {x: 0});
-        expect(html).to.contain(`<div class="baz">`);
-
-        html = renderFixture(`code`, {x: -1});
-        expect(html).not.to.contain(`<div class="baz">`);
-      });
-
-      it(`outputs inline buffered code`, function() {
-        expect(html).to.contain(`<div class="inline-script"><div class="raw-inline">within another div`);
-      });
-
-      it(`outputs standalone buffered code`, function() {
-        expect(html).to.contain(`<div class="raw-buffered">raw so raw`);
-      });
-
-      it(`does not output unbuffered code`, function() {
-        expect(html).not.to.contain(`should not be output`);
-      });
-    });
-  });
-}
+  }
+});
 
 describe(`snabbdom-specific rendering`, function() {
   const html = fixtureToHTML(`snabb`, {}, {
@@ -332,30 +363,5 @@ describe(`snabbdom-specific rendering`, function() {
 
   it(`renders object-only class notation`, function() {
     expect(html).to.contain(`<a class="aclass"`);
-  });
-});
-
-describe(`Render`, function() {
-  jsdom();
-
-  describe(`multiple files`, function() {
-    it(`inserts included literal (non-jade) files`, function() {
-      const html = fixtureToHTML(`literal-import`);
-      const singleRootImport = `<div class="test">test</div>`;
-      const multiRootImport = `<div>child 1</div><div>child 2</div>`;
-      const htmlEntities = function(str) {
-        return String(str).replace(/&/g, `&amp;`).replace(/</g, `&lt;`).replace(/>/g, `&gt;`).replace(/"/g, `&quot;`);
-      };
-      expect(html).to.be(
-        `<div class="raw">` +
-          `<div class="single-root">` +
-            `<text>` + htmlEntities(singleRootImport) + `</text>` +
-          `</div>` +
-          `<div class="multi-root">` +
-            `<text>` + htmlEntities(`<div>` + multiRootImport + `</div>`) + `</text>` +
-          `</div>` +
-        `</div>`
-      );
-    });
   });
 });
